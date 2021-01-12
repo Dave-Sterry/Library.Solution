87b7a5f700728b592f8374769432a4aa30de710a
kevinkirkl3y <kevinmkirkley@gmail.com>
2021-01-12 10:56:15 -0800

    Add BooksController.
    Co-authored-by: Shannon Grantski <grantski@pm.me>
    Co-authored-by: David Sterry <sterry.david@gmail.com>

--- Library/Models/Author.cs
+++ Library/Models/Author.cs
@@ -10,12 +10,19 @@ namespace Library.Models
       this.AuthorBooks = new HashSet<AuthorBook>();
     }
     public int AuthorId { get; set; }
-    public string Name { get; set; }
-    public string FirstName { get; set; }
-    public string MidIn { get; set; }
+    private string _name;
+    
+    public string Name 
+    {
+      get { return FirstName + " " + MidIn + " " + LastName; }
+      set { _name = value; } 
+    }
+    public  string FirstName { get; set; }
+    public  string MidIn { get; set; }
     public string LastName { get; set; }
     public ICollection<Book> Books { get; }
     public ICollection<AuthorBook> AuthorBooks { get; }
+
   }
 
 }
